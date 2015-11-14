07ac5fec6cbd0d695a5870c1b360e40d15037eec
David Ellinger <david.ellinger@me.com>
2015-11-13 19:56:33 -0800

    Removed all files.

--- models/RealmAuctionIndex.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = function(sequelize, DataTypes) {
-    var RealmAuctionIndex = sequelize.define("RealmAuctionIndex", {
-        url: DataTypes.STRING,
-        lastModified: DataTypes.DATE
-    });
-    return RealmAuctionIndex;
-}
--- public/javascripts/config.js
+++
@@ -1,7 +0,0 @@
-angular.module("ahNotifier.config", [])
-
-.constant("environment", {
-	"rest_api_uri": "http:localhost:3000/api"
-})
-
-;
