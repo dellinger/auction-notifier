97b0f363df63cbf73109e32fdc1bfbc316b696b0
David Ellinger <david.ellinger@me.com>
2015-11-22 22:36:18 -0800

    Created a quick bgulp task to spool up a dev server and watch for any changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,19 @@ var gulp = require('gulp');
 var tsd = require('gulp-tsd');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
+var nodemon = require('gulp-nodemon')
 
-gulp.task('build', ['tsd', 'scripts', 'copy'], function(cb) {
+
+gulp.task('build', ['tsd', 'scripts', 'copy'], function(cb) {});
+gulp.task('dev-server', ['start']);
+
+gulp.task('start', function () {
+    nodemon({
+        script: 'dist/server.js'
+        , ext: 'js html'
+        , env: { 'NODE_ENV': 'development' },
+        tasks: ['build']
+    })
 });
 
 gulp.task('tsd', function (callback) {
@@ -25,3 +36,6 @@ gulp.task('copy', function () {
         .src('config/*.json')
         .pipe(gulp.dest('dist/config'))
 });
+
+
+
